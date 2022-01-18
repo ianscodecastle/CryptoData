b4784e1eecdfca62577529fadcf2e71b927abbbf
Ian Robinson <ianrobinson171@gmail.com>
2022-01-18 12:37:31 -0600

    added keys and urls  Please enter the commit message for your changes. Lines starting

---
+++ main.py
@@ -0,0 +1,7 @@
+
+api_domain = 'pro-api.coinmarketcap.com'
+my_key = '197d8494-cdfe-4ca3-aece-1aa263bed70b'
+
+api_sandbox = 'sandbox-api.coinmarketcap.com'
+test_key = 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+
