09dd55d9091138ea3dbec262d1d9f96ef223b76a
Ian Robinson <ianrobinson171@gmail.com>
2022-01-25 11:05:27 -0600

    added sandbox urls

--- main.py
+++ main.py
@@ -5,13 +5,30 @@ import json
 my_key = '197d8494-cdfe-4ca3-aece-1aa263bed70b'
 sandbox_key = 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
 
-url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+# Sandbox URLs 
+latest_listings = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+latest_quotes = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+gainers_losers_trending = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/trending/gainers-losers'
+price_preformance = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/price-performance-stats/latest'
+
+
+# url = latest_listings
+# parameters = {
+#   'start':'1',
+#   'limit':'3',
+#   'convert':'USD'
+# }
+
+url = latest_quotes
 parameters = {
-  #'start':'1',
   'id':'1',
-  #'limit':'3',
+  'aux':'btc_dominance',
   'convert':'USD'
 }
+
+
+
+
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': sandbox_key,
