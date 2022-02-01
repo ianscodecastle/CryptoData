50dd98ce76ca63cd05bcdf3676801b186e8fda28
Ian Robinson <ianrobinson171@gmail.com>
2022-02-01 14:58:43 -0600

    minor changes

--- data_extract.py
+++ data_extract.py
@@ -2,6 +2,7 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
+# API Keys
 my_key = '197d8494-cdfe-4ca3-aece-1aa263bed70b'
 sandbox_key = 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
 
@@ -11,23 +12,13 @@ latest_quotes = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/
 gainers_losers_trending = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/trending/gainers-losers'
 price_preformance = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/price-performance-stats/latest'
 
-
+# Show latest listings
 url = latest_listings
 parameters = {
   'start':'1',
   'limit':'3',
   'convert':'USD'
 }
-
-# url = latest_quotes
-# parameters = {
-#   'id':'1',
-#   'aux':'btc_dominance',
-#   'convert':'USD'
-# }
-
-
-
 
 headers = {
   'Accepts': 'application/json',
