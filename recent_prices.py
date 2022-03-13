8da0da76a2080329c2d390fe43ae8123711a0a35
ianscodecastle <ianrobinson171@gmail.com>
2022-03-12 20:08:01 -0600

    made seperate files for endpoints

---
+++ recent_prices.py
@@ -0,0 +1,35 @@
+# Imports
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+# API Keys
+my_key = '197d8494-cdfe-4ca3-aece-1aa263bed70b'
+sandbox_key = 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+
+# URL for recent coin prices
+latest_quotes = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+# Get quote
+url = latest_quotes
+parameters = {
+  'slug': 'btc',  
+  'convert':'USD'
+}
+
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': sandbox_key,
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+#   data = json.loads(response.text)
+#   print(data)
+  print(response.text)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
