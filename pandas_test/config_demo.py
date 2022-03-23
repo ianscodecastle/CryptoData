99fb19e388437999bd129a62f709dd06c75a1b35
ianscodecastle <ianrobinson171@gmail.com>
2022-03-22 22:26:26 -0500

    worked with pandas

---
+++ pandas_test/config_demo.py
@@ -0,0 +1,9 @@
+import pandas
+import requests
+import config_demo
+
+# cmc test key
+api_key = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462"
+
+#urls
+latest_quotes = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
