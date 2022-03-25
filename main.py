e2bce96b6ba670b2230eec915502aff8a5a990a1
ianscodecastle <ianrobinson171@gmail.com>
2022-03-24 23:13:25 -0500

    header art

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ link_logo = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64
 dot_logo = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png'))
 ada_logo = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png'))
 
+header_image = Image.open(urlopen('https://images.ctfassets.net/0idwgenf7ije/6OUAGu0C76BTCRaJ9t9rs7/9bcfc6d0ae3c0247a2d1b87157f964a3/Gemini-Non-Fungible_Token-_One_of_a_Kind_Assets_for_Collectibles_and_Rare_Items.png?fm=webp'))
+
+st.image(header_image)
 st.title('Crypto Prices Today')
 st.write("")
 #st.header('My Coins')
