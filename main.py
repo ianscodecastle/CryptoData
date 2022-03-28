08c1054e4610ddc3e1ced1960f7443d8a2256571
Ian Robinson <ianrobinson171@gmail.com>
2022-03-28 14:59:39 -0500

    experimenting w pipeline in main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+# imports
+import json
 import string
-from nbformat import write
-import streamlit as st
-from PIL import Image
-from etl import data_extract
-from assets import images
 from urllib.request import urlopen
+import pandas as pd
+import streamlit as st
+from nbformat import write
+from PIL import Image
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from assets import images
+from etl import config, data_extract
 
 def buildHeader():
     st.image(images.header_image)
@@ -72,5 +77,50 @@ def buildPages():
         st.subheader('Market Cap Dominance')
         st.area_chart(data_extract.mkd)
 
+def extractData():
+    # Show latest listings
+    url = config.sandbox_latest_quotes
+    parameters = {
+    'slug':'bitcoin,ethereum,chainlink,polkadot,cardano',
+    'convert':'USD'
+    }
+
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': config.sandbox_key,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+    response = session.get(url, params=parameters)
+
+    # Specify which data from API response to store
+    data = json.loads(response.text)['data']
+
+    # Make data frame
+    df = pd.DataFrame(data).T
+    df = pd.json_normalize(list(data.values()))
+    df.index = data.keys()
+
+    # Display dataframe
+    #display(df)
+    df.rename(columns={'name': 'NAME', 'quote.USD.price':'Price (USD)','quote.USD.percent_change_24h':'24h Change (%)', 'quote.USD.percent_change_7d':'7d Change (%)', 'quote.USD.market_cap':'Market Cap (USD)', 'quote.USD.market_cap_dominance':'Market Cap Dominance'}, inplace=True)
+
+    # Create views
+    df_view1 = df[['slug', 'Price (USD)']] # all rows, specific columns
+
+    core_info = ['Price (USD)','24h Change (%)','7d Change (%)','Market Cap (USD)']
+    df_btc = df.loc[['bitcoin'], core_info]
+    df_eth = df.loc[['ethereum'], core_info]
+    df_link = df.loc[['chainlink'], core_info]
+    df_dot = df.loc[['polkadot'], core_info]
+    df_ada = df.loc[['cardano'], core_info]
+
+    movers_24h = df.loc[:,['24h Change (%)']]
+    movers_7d = df.loc[:,['7d Change (%)']]
+
+    mkd = df.loc[:,['Market Cap Dominance']]
+
+extractData()
 buildHeader()
 buildPages()
